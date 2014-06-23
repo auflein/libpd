dc436b1ae871cb53f6579114683c7aba4c9d175b
Luke Iannini <lukexi@me.com>
2014-06-23 04:11:23 -0700

    Add PdOSX dir

---
+++ PdOSX/PdOSX.h
@@ -0,0 +1,24 @@
+//
+//  PdOSX.h
+//  PdOSX
+//
+//  Created by Luke Iannini on 6/20/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PdOSX.
+FOUNDATION_EXPORT double PdOSXVersionNumber;
+
+//! Project version string for PdOSX.
+FOUNDATION_EXPORT const unsigned char PdOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PdOSX/PublicHeader.h>
+
+
+#import <PdOSX/PdAudioUnit.h>
+#import <PdOSX/PdBase.h>
+#import <PdOSX/PdFile.h>
+#import <PdOSX/PdDispatcher.h>
+#import <PdOSX/PdMidiDispatcher.h>
